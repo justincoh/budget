16ebfb4c4f7e67c542291ebc8b88249cf29e0305
Justin Cohen <justin.cohen@handshake.com>
2015-09-29 17:16:33 -0400

    reformates r&d category data, adds react dependency

--- public/js/temp.js
+++ public/js/temp.js
@@ -2,5 +2,18 @@
 d3.csv('data/hist04z1_outlayByAgency1.csv',function(err,data){
 	let tempObj = {};
 	if(err){return console.log('ERROR ',err)}
-	data.forEach(record => {})
+	console.log(data)
+	// data.forEach(record => {console.log})
+});
+
+d3.csv('data/hist09z8_RnDByCategory.csv',function(d){
+	return {
+
+	}
+
+	}, function(err,rows){
+	let tempObj = {};
+	if(err){return console.log('ERROR ',err)}
+	console.log('Rnd by category ',rows);
+	// rows.forEach(record => {console.log})
 });
