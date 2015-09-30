58b0f3ba49bd4bb8a0d7f7aba7fd5a12e8ef10be
Justin Cohen <justin.cohen@handshake.com>
2015-09-30 18:49:09 -0400

    adds webpack, reformats csvs, adds cdns for react

--- public/js/temp.js
+++ public/js/temp.js
@@ -1,19 +1,28 @@
 'use strict';
-d3.csv('data/hist04z1_outlayByAgency1.csv',function(err,data){
-	let tempObj = {};
-	if(err){return console.log('ERROR ',err)}
-	console.log(data)
-	// data.forEach(record => {console.log})
+var i = 0;
+d3.csv('data/hist04z1_outlayByAgency1.csv', function(d) {
+    for (let key in d) {
+        if (key[0] === '1' || key[0] === '2') {
+            d[key] = parseInt(d[key]) //string --> number
+        }
+    }
+    return d; //transforms go here
+
+}, function(err, data) {
+    let tempObj = {};
+    if (err) {
+        return console.log('ERROR ', err)
+    }
+    console.log('Outlay By Agency', data);
+    // data.forEach(record => {console.log})
 });
 
-d3.csv('data/hist09z8_RnDByCategory.csv',function(d){
-	return {
+// d3.csv('data/hist09z8_RnDByCategory.csv',function(d){
+// 	return d; //transforms go here
 
-	}
-
-	}, function(err,rows){
-	let tempObj = {};
-	if(err){return console.log('ERROR ',err)}
-	console.log('Rnd by category ',rows);
-	// rows.forEach(record => {console.log})
-});
+// 	}, function(err,data){
+// 	let tempObj = {};
+// 	if(err){return console.log('ERROR ',err)}
+// 	console.log('Rnd by category ',data);
+// 	// data.forEach(record => {console.log})
+// });
