3483b6159c9583fc8dd8c8fa62348b48028723b0
justin cohen <justincoh@gmail.com>
2020-04-07 10:18:56 -0400

    generate json files

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 import csv
+import json
+import pandas as pd
 
 
-def read_csv(filename):
-  for_return = []
-  with open(filename, 'r') as infile:
-    reader = csv.DictReader(infile)
-      for row in reader:
-        for_return.append(row)
+def csv_to_dict_keyed_by_year(filename):
+  return pd.read_csv(
+    filename,
+    index_col=0,
+    squeeze=True
+  ).to_dict()
 
-  return for_return
+def csv_to_dict_keyed_by_dept(filename):
+  return pd.read_csv(
+    filename,
+    index_col=0,
+    squeeze=True
+  ).transpose().to_dict()
 
-# TODO turn this into JSON and save as a blob for use in frontend
+def write_dict_to_json(data, filename):
+  with open(filename, 'w') as outfile:
+    json.dump(data, filename)
