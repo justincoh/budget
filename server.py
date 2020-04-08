048a199fd8f92812591b9217d66abda7481e5d6d
justin cohen <justincoh@gmail.com>
2020-04-08 19:43:00 -0400

    gets api right, renders basic chart

--- server.py
+++ server.py
@@ -1,11 +1,19 @@
-from flask import Flask, render_template, request
+from flask import (
+  Flask,
+  render_template,
+  request,
+  jsonify
+)
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+  return render_template('index.html')
 
 @app.route('/data/<filename>')
 def get_json_data(filename):
-    # grab json file
-    return {}
+  # grab json file
+  with open(filename, 'r') as infile:
+    data = infile.read()
+
+  return jsonify(data)
--- static/index.js
+++ static/index.js
@@ -1,5 +1,44 @@
+function makeChart (data) {
+  const one = data["Total outlays"]; // for dev
+
+  const ctx = document.getElementById("chart");
+  return new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: Object.keys(one),
+      datasets: [{
+      label: "Test dataset",
+        backgroundColor: "rgb(143, 0, 143)",
+        data: Object.values(one),
+      }],
+    },
+    options: {
+      responsive: true,
+      // scales: {
+      //   x: {
+      //     display: true,
+      //     scaleLabel: {
+      //       display: true,
+      //       labelString: 'X Label'
+      //     }
+      //   },
+      //   y: {
+      //     display: true,
+      //     scaleLabel: {
+      //       display: true,
+      //       labelString: 'Y Label'
+      //     }
+      //   }
+      // }
+    },
+  });
+}
 document.addEventListener("DOMContentLoaded", () => {
   fetch("data/outlays_by_agency_dept_keyed.json")
     .then(data => data.json())
-    .then(data => console.log(data));
+    .then((blob) => {
+      const data = JSON.parse(blob);
+      window.data = data;
+      makeChart(data);
+    });
 });
