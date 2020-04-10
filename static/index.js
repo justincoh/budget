e483ea0d42244a7ffb50ed807907526187af860a
justin cohen <justincoh@gmail.com>
2020-04-09 23:05:14 -0400

    more updates, nothing big

--- static/index.js
+++ static/index.js
@@ -1,6 +1,6 @@
+function getRandomColor () { return '#'+Math.floor(Math.random()*16777215).toString(16); }
 // relies on data being global
 function generateDataset (key) {
-  const randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
   return {
     label: key,
     fill: false,
@@ -10,8 +10,8 @@ function generateDataset (key) {
   }
 }
 
-const depts = [
-  // "Department of Defense--Military Programs",
+const DEPARTMENTS = [
+  "Department of Defense--Military Programs",
   "Department of Education",
   "Department of Homeland Security",
   "National Aeronautics and Space Administration",
@@ -25,14 +25,7 @@ function makeChart (data) {
     type: "line",
     data: {
       labels: Object.keys(one),
-      datasets: depts.map(dep => generateDataset(dep)),
-      // datasets: [{
-      //   label: "Test dataset",
-      //   fill: false,
-      //   backgroundColor: "purple",
-      //   borderColor: "purple",
-      //   data: Object.values(one),
-      // }],
+      datasets: DEPARTMENTS.map(dep => generateDataset(dep)),
     },
     options: {
       responsive: true,
@@ -55,12 +48,61 @@ function makeChart (data) {
     },
   });
 }
+
+function generateBarData (yearData, key) {
+  return {
+    barPercentage: 1,
+    categoryPercentage: 1,
+    backgroundColor: getRandomColor(),
+    data: [yearData[key]],
+    label: key,
+  };
+}
+
+// why does Chartjs assume bar chart data is going to be time series?
+// That's not the point of bar charts
+// I think D3 is probably a better choice for that one :(
+function makeBarChartForYear(data) {
+
+}
+
+function makePolarAreaChartForYear(data) {
+  const ctx = document.getElementById("chart");
+  const yearData = data["2018"];
+  // filter to get e.g. { "Department of Education": 12345}
+  // const barData = DEPARTMENTS.map(dept => yearData[dept]);
+  return new Chart(ctx, {
+    type: "polarArea",
+    data: {
+      datasets: [{
+        data: DEPARTMENTS.map(dep => yearData[dep]),
+        backgroundColor: DEPARTMENTS.map(() => getRandomColor()),
+      }],
+      labels: DEPARTMENTS,
+    },
+    options: {
+      responsive: true,
+			title: {
+				display: true,
+				text: "Testing a Polar Chart",
+			},
+		}
+  });
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  fetch("data/outlays_by_agency_dept_keyed.json")
+  // fetch("data/outlays_by_agency_dept_keyed.json")
+  //   .then(data => data.json())
+  //   .then((blob) => {
+  //     const data = JSON.parse(blob);
+  //     window.data = data;
+  //     makeChart(data);
+  //   });
+  fetch("data/outlays_by_agency_year_keyed.json")
     .then(data => data.json())
     .then((blob) => {
       const data = JSON.parse(blob);
       window.data = data;
-      makeChart(data);
+      makeBarChartForYear(data);
     });
 });
